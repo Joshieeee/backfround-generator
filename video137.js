bc08542954bf3d049b579b86a93d32b41ce9b8bb
Josh H <51180404+Joshieeee@users.noreply.github.com>
2020-12-15 17:12:22 -0800

    adding starting files

---
+++ video137.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btnRandom = document.getElementById("random")
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+	css.textContent = body.style.background + ";";
+}
+
+function randomize() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+
+css.textContent = "linear-gradient(to right, rgb(0, 255, 0), rgb(255,0,0));"
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+btnRandom.addEventListener("click", randomize);
